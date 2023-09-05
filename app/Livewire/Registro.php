03c6d4ba0483fd05549521116cbb7680bb9b0aeb
tlaxcalteco <pedro_1320114068@uptecamac.edu.mx>
2023-09-04 22:39:53 -0600

    CRUD

---
+++ app/Livewire/Registro.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Registro extends Component
+{
+    public function render()
+    {
+        return view('livewire.registro');
+    }
+}
